62f0cc9ef90c866198e9f3d34dba6faa9690b4b1
danellazs <114693616+danellazs@users.noreply.github.com>
2024-10-24 15:33:20 +0700

    Interface using C#
    experimental interface. Only a few button usable and not complete pages

---
+++ junpro test ui/junpro test ui/dashboard.Designer.cs
@@ -0,0 +1,69 @@
+﻿namespace junpro_test_ui
+{
+    partial class dashboard
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(dashboard));
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.Transparent;
+            button1.BackgroundImageLayout = ImageLayout.None;
+            button1.FlatAppearance.BorderColor = Color.White;
+            button1.FlatAppearance.BorderSize = 0;
+            button1.FlatAppearance.MouseDownBackColor = Color.Transparent;
+            button1.FlatAppearance.MouseOverBackColor = Color.Transparent;
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Font = new Font("Microsoft Tai Le", 8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button1.Location = new Point(74, 192);
+            button1.Name = "button1";
+            button1.Size = new Size(112, 34);
+            button1.TabIndex = 0;
+            button1.TextAlign = ContentAlignment.MiddleLeft;
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // dashboard
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+            ClientSize = new Size(1418, 1050);
+            Controls.Add(button1);
+            Name = "dashboard";
+            Text = "Form3";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button button1;
+    }
+}
---
+++ junpro test ui/junpro test ui/product.Designer.cs
@@ -0,0 +1,66 @@
+﻿namespace junpro_test_ui
+{
+    partial class product
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(product));
+            buttonMilk = new Button();
+            SuspendLayout();
+            // 
+            // buttonMilk
+            // 
+            buttonMilk.BackColor = Color.Transparent;
+            buttonMilk.FlatAppearance.BorderSize = 0;
+            buttonMilk.FlatAppearance.MouseDownBackColor = Color.Transparent;
+            buttonMilk.FlatAppearance.MouseOverBackColor = Color.Transparent;
+            buttonMilk.FlatStyle = FlatStyle.Flat;
+            buttonMilk.Location = new Point(286, 553);
+            buttonMilk.Name = "buttonMilk";
+            buttonMilk.Size = new Size(336, 485);
+            buttonMilk.TabIndex = 0;
+            buttonMilk.UseVisualStyleBackColor = false;
+            buttonMilk.Click += buttonMilk_Click;
+            // 
+            // product
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ControlLightLight;
+            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+            ClientSize = new Size(1419, 1050);
+            Controls.Add(buttonMilk);
+            Name = "product";
+            Text = "Form1";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button buttonMilk;
+    }
+}
